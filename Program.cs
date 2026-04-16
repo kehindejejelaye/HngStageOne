7a2cc51f06e2d97c0709945d893fa3cf04535cab
Kehinde Jejelaye <kehindejejelaye@gmail.com>
2026-04-16 19:15:29 +0100

    new change

--- Program.cs
+++ Program.cs
@@ -1,11 +1,11 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => Results.Ok(new { message = "API is running" }));
+app.MapGet("/", () => Results.Json(new { message = "API is running" }));
 
-app.MapGet("/health", () => Results.Ok(new { message = "healthy" }));
+app.MapGet("/health", () => Results.Json(new { message = "healthy" }));
 
-app.MapGet("/me", () => Results.Ok(new { 
+app.MapGet("/me", () => Results.Json(new { 
     name = "Kehinde Jejelaye", 
     email = "kehindejejelaye@gmail.com", 
     github = "https://github.com/kehindejejelaye" 
